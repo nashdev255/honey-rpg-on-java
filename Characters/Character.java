ed95134a0cb5609206f24ef6e894596beda7980c
nashdev255 <yutori1107@gmail.com>
2023-09-20 23:12:03 +0900

    add printStatus and javadocs

--- Characters/Character.java
+++ Characters/Character.java
@@ -1,13 +1,35 @@
+/**
+ * @file Character.java
+ * @author nashdev255
+ */
+
 package Characters;
 
+/**
+ * The base class of Fighter, Monster.
+ */
 public class Character {
   private String name;
+  private int hp, maxhp, atk, def, spd;
 
-  public Character(String name) {
+  public Character(String name, int maxhp, int atk, int def, int spd) {
     this.name = name;
+    this.maxhp = maxhp;
+    this.hp = this.maxhp;
+    this.atk = atk;
+    this.def = def;
+    this.spd = spd;
   }
 
-  public String getName() {
-    return name;
+  /**
+   * Show every status which character has.
+   */
+  public void printStatus() {
+    System.out.println("<Status>");
+    System.out.println("NAME : " + this.name);
+    System.out.println("HP : " + this.hp + '/' + this.maxhp);
+    System.out.println("ATK : " + this.atk);
+    System.out.println("DEF : " + this.def);
+    System.out.println("SPD : " + this.spd);
   }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
+/**
+ * @file Main.java
+ * @author nashdev255
+ * @brief Compile this file and execute to demonstrate.
+ */
+
 import Characters.Character;
 
 class Main {
   public static void main(String[] Args) {
-    Character character = new Character("Lumine");
-    System.out.println(character.getName());
+    Character character = new Character("Lumine", 1000, 100, 100, 100);
+    character.printStatus();
   }
 }
