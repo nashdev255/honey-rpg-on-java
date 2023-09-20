589f1fd6174c6d24cf4174b2120d52d3246f0d46
nashdev255 <yutori1107@gmail.com>
2023-09-20 23:46:29 +0900

    add javadocs to Monster

--- Characters/Monsters/Monster.java
+++ Characters/Monsters/Monster.java
@@ -1,7 +1,15 @@
+/**
+ * @file Monster.java
+ * @author nashdev255
+ */
+
 package Characters.Monsters;
 
 import Characters.Character;
 
+/**
+ * Define opponents against fighters.
+ */
 public class Monster extends Character {
   public Monster(String name, int maxhp, int atk, int def, int spd, int level) {
     super(name, maxhp, atk, def, spd, level);
