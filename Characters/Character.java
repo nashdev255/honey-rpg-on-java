b4c75d00a8a50a8d24982750a69e056df3afc6db
nashdev255 <yutori1107@gmail.com>
2023-09-20 23:59:04 +0900

    change Character to abstract

--- Characters/Character.java
+++ Characters/Character.java
@@ -8,10 +8,10 @@ package Characters;
 /**
  * The base class of Fighter, Monster.
  */
-public class Character {
-  private String name;
-  private int hp, maxhp, atk, def, spd;
-  private int level, minLevel = 1, maxLevel = 100;
+public abstract class Character {
+  protected String name;
+  protected int hp, maxhp, atk, def, spd;
+  protected int level, minLevel = 1, maxLevel = 100;
 
   public Character(String name, int maxhp, int atk, int def, int spd, int level) {
     this.name = name;
@@ -26,15 +26,5 @@ public class Character {
   /**
    * Show every status which character has.
    */
-  public void printStatus() {
-    System.out.println("====================");
-    System.out.println("<Status>");
-    System.out.println("NAME : " + this.name);
-    System.out.println("LEVEL : " + this.level);
-    System.out.println("HP : " + this.hp + '/' + this.maxhp);
-    System.out.println("ATK : " + this.atk);
-    System.out.println("DEF : " + this.def);
-    System.out.println("SPD : " + this.spd);
-    System.out.println("====================");
-  }
+  public abstract void printStatus();
 }
--- Characters/Fighters/Fighter.java
+++ Characters/Fighters/Fighter.java
@@ -11,7 +11,22 @@ import Characters.Character;
  * Users operate their own fighters created according to this class.
  */
 public class Fighter extends Character {
-  public Fighter(String name, int maxhp, int atk, int def, int spd, int level) {
+  private String codeName;
+
+  public Fighter(String name, int maxhp, int atk, int def, int spd, int level, String codeName) {
     super(name, maxhp, atk, def, spd, level);
+    this.codeName = codeName;
+  }
+
+  public void printStatus() {
+    System.out.println("====================");
+    System.out.println("<Status>");
+    System.out.println("NAME : " + this.name);
+    System.out.println("LEVEL : " + this.level);
+    System.out.println("HP : " + this.hp + '/' + this.maxhp);
+    System.out.println("ATK : " + this.atk);
+    System.out.println("DEF : " + this.def);
+    System.out.println("SPD : " + this.spd);
+    System.out.println("====================");
   }
 }
